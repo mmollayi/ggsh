aa2818aaf75e709595f0672e4486f557893ccdee
mmollayi <mmollayi@gmail.com>
2026-02-17 18:56:37 +0330

    new tests

--- tests/testthat/test-breaks.R
+++ tests/testthat/test-breaks.R
@@ -7,3 +7,9 @@ test_that("breaks_pretty() dispatches to pretty.jdatetime() for jdatetime inputs
     x <- jdatetime_make(1404, 11, 23:24, tzone = "Asia/Tehran")
     expect_s3_class(scales::breaks_pretty(n = 2)(x), "jdatetime")
 })
+
+test_that("breaks_width_ggsh() can take date units for jdate inputs", {
+    x <- jdatetime_make(1404, 11, 23:24, tzone = "Asia/Tehran")
+    expect_s3_class(scales::breaks_pretty(n = 2)(x), "jdatetime")
+})
+
--- tests/testthat/test-fullseq.R
+++ tests/testthat/test-fullseq.R
@@ -1,3 +1,13 @@
+test_that("fullseq() dispatches to fullseq.jdate() for jdate inputs", {
+    x <- jdate_make(1404, day = 23:24)
+    expect_s3_class(scales::fullseq(x, "days"), "jdate")
+})
+
+test_that("fullseq() dispatches to fullseq.jdatetime() for jdatetime inputs", {
+    x <- jdatetime_make(1404, second = 1:2, tzone = "Asia/Tehran")
+    expect_s3_class(scales::fullseq(x, "secs"), "jdatetime")
+})
+
 test_that("fullseq works for jdate as expected", {
     x <- shide::jdate("1403-01-01") + 1:30
     expect_equal(fullseq(x, "1 month"), shide::jdate(c("1403-01-01", "1403-02-01")))
