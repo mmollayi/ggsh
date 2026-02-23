36385e2bf0e7458d9c25843d4a01ea76fd15218f
mmollayi <mmollayi@gmail.com>
2026-02-23 18:16:07 +0330

    change function name

--- tests/testthat/test-label.R
+++ tests/testthat/test-label.R
@@ -1,7 +1,7 @@
-test_that("date_format works correctly", {
-    d <- jdate("1404-11-24")
+test_that("label_jdate() works as expected for jdate input", {
+    x <- jdate("1404-11-24")
 
-    expect_equal(label_jdate()(d), "1404-11-24")
-    expect_equal(label_jdate(format = "%J")(d), "1404/11/24")
+    expect_equal(label_jdate()(x), "1404-11-24")
+    expect_equal(label_jdate(format = "%J")(x), "1404/11/24")
     expect_equal(label_jdate()(jdate(NA_real_)), NA_character_)
 })
